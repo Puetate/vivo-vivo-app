12a3eec4c1925fd7151da771622ba580e03690a0
Pablo <jhoelpablo02@gmail.com>
2023-11-03 15:52:15 -0500

    add route generator

--- lib/src/routes/route_generator.dart
+++ lib/src/routes/route_generator.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:vivo_vivo_app/src/screens/Home/home_view.dart';
 import 'package:vivo_vivo_app/src/screens/Login/login_view.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final Map<String, dynamic Function(BuildContext)> routes = {
-      "/": (context) => const LoginView(),
-      "/homePage": (context) => const LoginView(),
+      LoginView.id: (_) => const LoginView(),
+      HomeView.id: (_) => const HomeView(),
     };
 
     final routeBuilder = routes[settings.name];
     if (routeBuilder != null) {
-      return MaterialPageRoute(builder: (context) => routeBuilder(context),settings: settings);
+      return MaterialPageRoute(
+        builder: (context) => routeBuilder(context),
+        settings: settings,
+      );
     }
 
     return MaterialPageRoute(builder: (context) => const LoginView());
